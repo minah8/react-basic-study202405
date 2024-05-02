3aaa04846831d08f0d53552e2a3c0256848a8912
minah8 <o_uo_o@naver.com>
2024-05-02 16:56:46 +0900

    8. 동적 리스트 렌더링, 연도별 필터링

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ function App() {
   //지출 항목 객체 배열
   const expenses = [
     {
+      id: 1,
       title: '바나나',
       price: 2000,
       date: new Date(2023, 3 - 1, 23),
     },
     {
+      id: 2,
       title: 'BBQ치킨',
       price: 20000,
-      date: new Date(2023, 5 - 1, 21),
+      date: new Date(2022, 5 - 1, 21),
     },
     {
+      id: 3,
       title: '도미노피자',
       price: 35000,
-      date: new Date(2023, 7 - 1, 4),
+      date: new Date(2021, 7 - 1, 4),
+    },
+    {
+      id: 4,
+      title: '엽기떡볶이',
+      price: 18000,
+      date: new Date(2024, 5 - 1, 1),
     },
   ];
 
