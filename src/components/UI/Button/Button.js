64ed5d53c3ff37bf4c2b236f5572f285b86a6aad
minah8 <o_uo_o@naver.com>
2024-05-03 16:03:02 +0900

    Users App 기본 템플릿 작성

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,34 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-// import './Button.css';
+import './Button.css';
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #f1baa1;
-  color: white;
-  background: #f1baa1;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #f1baa1;
+//   color: white;
+//   background: #f1baa1;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #bd613c;
-    border-color: #bd613c;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #bd613c;
+//     border-color: #bd613c;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-// const Button = ({ type, onClick, children }) => {
-//   return (
-//     <button type={type} onClick={onClick} className="button">
-//       {children}
-//     </button>
-//   );
-// };
+const Button = ({ type, onClick, children }) => {
+  return (
+    <button type={type} onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+};
 
 export default Button;
