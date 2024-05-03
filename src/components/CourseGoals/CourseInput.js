dba31a050a50422e210c6e12c614547f9bda6fb8
minah8 <o_uo_o@naver.com>
2024-05-03 14:36:56 +0900

    목표설정 App - 기본 기능(목록, 추가, 삭제)

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = () => {
+const CourseInput = ({ onAdd }) => {
+  const [enteredText, setEnteredText] = useState('');
+
+  const textChageHandler = (e) => {
+    setEnteredText(e.target.value);
+  };
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    onAdd(enteredText);
+    setEnteredText('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>나의 목표</label>
-        <input type="text" />
+        <input type="text" onChange={textChageHandler} value={enteredText} />
       </div>
       <Button type="submit">목표 추가하기</Button>
     </form>
