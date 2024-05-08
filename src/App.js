a32da36c53f24a6c2366dcaaa07026aca494e28b
minah8 <o_uo_o@naver.com>
2024-05-08 13:01:17 +0900

    Cart Modal 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Cart from './components/Food/Cart/Cart';
 import Header from './components/Food/Layout/Header';
 import Meals from './components/Food/Meals/Meals';
 
 const App = () => {
   return (
     <>
+      <Cart />
       <Header />
       <div id="main">
         <Meals />
