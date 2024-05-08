9ac99c4716ae9619d7f08f743ae85295dc50b939
minah8 <o_uo_o@naver.com>
2024-05-08 12:23:42 +0900

    Food 예제 템플릿 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Food/Layout/Header';
+import Meals from './components/Food/Meals/Meals';
 
 const App = () => {
   return (
     <>
       <Header />
+      <div id="main">
+        <Meals />
+      </div>
     </>
   );
 };
--- src/components/Food/Layout/Header.js
+++ src/components/Food/Layout/Header.js
@@ -10,7 +10,6 @@ const Header = () => {
       <header className={styles.header}>
         <h1>ReactMeals</h1>
         <HeaderCartButton />
-        <button>Cart</button>
       </header>
       <div className={mainImage}>
         <img
